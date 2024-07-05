ef25743fd4c2e465408881930e2784880375f250
Hector <hector.m.romero.ugalde@gmail.com>
2024-07-05 10:12:35 +0200

    mlflow running with minio and postgre

--- roug_ml/configs/run_mlflow.py
+++ roug_ml/configs/run_mlflow.py
@@ -5,7 +5,12 @@ Script to run MLflow Tracking Server
 import os
 from dotenv import load_dotenv
 
-load_dotenv("/Users/hector/cancer_subtype_prediction/roug_ml/roug_ml/configs/mlflow.env")
+load_dotenv("/home/hector/roug/cancer_subtype_prediction/roug_ml/roug_ml/configs/mlflow.env")
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://127.0.0.1:9000"
+os.environ["AWS_ACCESS_KEY_ID"] = "kensou"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "H03042020P16082022"
+
 # Construct the mlflow server command with the desired artifact path and PostgreSQL backend store
 mlflow_command = (
     "mlflow server --backend-store-uri postgresql://mlflow_user:H03042020P16082022@localhost/mlflow_db "
